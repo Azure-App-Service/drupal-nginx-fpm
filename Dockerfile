8238339ea7ff2b13c4990eff6da0c9195a9ed51a
leonzhang77 <zhangjianfeng73@hotmail.com>
2018-01-17 06:10:05 +0000

    Fix restart issue,update Dockerfle/entrypoint.sh,remove unnecessary files.

--- Dockerfile
+++ Dockerfile
@@ -6,9 +6,21 @@ MAINTAINER Azure App Service Container Images <appsvc-images@microsoft.com>
 # ========
 #
 ENV DOCKER_BUILD_HOME "/dockerbuild"
-# wordpress 
+# drupal 
 ENV DRUPAL_SOURCE "/usr/src/drupal" 
-ENV DRUPAL_HOME "/home/site/wwwroot" 
+ENV DRUPAL_HOME "/home/site/wwwroot"
+# mariadb
+ENV MARIADB_DATA_DIR "/home/data/mysql"
+ENV MARIADB_LOG_DIR "/home/LogFiles/mysql"
+# phpmyadmin
+ENV PHPMYADMIN_SOURCE "/usr/src/phpmyadmin"
+ENV PHPMYADMIN_HOME "/home/phpmyadmin"
+#nginx
+ENV NGINX_LOG_DIR "/home/LogFiles/nginx"
+#php
+ENV PHP_HOME "/etc/php/7.0"
+ENV PHP_CONF_DIR $PHP_HOME"/cli"
+ENV PHP_CONF_FILE $PHP_CONF_DIR"/php.ini" 
 
 # ====================
 # Download and Install
@@ -24,17 +36,16 @@ WORKDIR $DOCKER_BUILD_HOME
 # -------------
 RUN  mkdir -p $DRUPAL_SOURCE 
 COPY drupal.tar.gz $DRUPAL_SOURCE/
-    
+
+# =========
+# Configure
+# =========
+WORKDIR $DRUPAL_HOME
+RUN rm -rf $DOCKER_BUILD_HOME
+
 # =====
 # final
 # =====
-WORKDIR $DRUPAL_HOME
-RUN rm -rf $DOCKER_BUILD_HOME
-# php
-COPY php.ini /etc/php/7.0/cli/php.ini
-# nginx
-COPY nginx.conf /etc/nginx/nginx.conf
-
 COPY entrypoint.sh /usr/local/bin/
 RUN chmod +x /usr/local/bin/entrypoint.sh
 
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,37 +1,113 @@
 #!/bin/bash
 
-set -e
+# set -e
 
 php -v
+setup_mariadb_data_dir(){
+    test ! -d "$MARIADB_DATA_DIR" && echo "INFO: $MARIADB_DATA_DIR not found. creating ..." && mkdir -p "$MARIADB_DATA_DIR"
+
+    # check if 'mysql' database exists
+    if [ ! -d "$MARIADB_DATA_DIR/mysql" ]; then
+	    echo "INFO: 'mysql' database doesn't exist under $MARIADB_DATA_DIR. So we think $MARIADB_DATA_DIR is empty."
+	    echo "Copying all data files from the original folder /var/lib/mysql to $MARIADB_DATA_DIR ..."
+	    cp -R --no-clobber /var/lib/mysql/. $MARIADB_DATA_DIR
+    else
+	    echo "INFO: 'mysql' database already exists under $MARIADB_DATA_DIR."
+    fi
+
+    rm -rf /var/lib/mysql
+    ln -s $MARIADB_DATA_DIR /var/lib/mysql
+    chown -R mysql:mysql $MARIADB_DATA_DIR
+    test ! -d /run/mysqld && echo "INFO: /run/mysqld not found. creating ..." && mkdir -p /run/mysqld
+    chown -R mysql:mysql /run/mysqld
+}
+
+start_mariadb(){
+    service mysql start
+
+    rm -f /tmp/mysql.sock
+    ln -s /var/run/mysqld/mysqld.sock /tmp/mysql.sock
+
+    # create default database 'azurelocaldb'
+    mysql -u root -e "CREATE DATABASE IF NOT EXISTS azurelocaldb; FLUSH PRIVILEGES;"
+}
+#unzip phpmyadmin
+setup_phpmyadmin(){
+    test ! -d "$PHPMYADMIN_HOME" && echo "INFO: $PHPMYADMIN_HOME not found. creating..." && mkdir -p "$PHPMYADMIN_HOME"
+    cd $PHPMYADMIN_SOURCE
+    tar -xf phpMyAdmin.tar.gz -C $PHPMYADMIN_HOME/ --strip-components=1    
+	cp -R phpmyadmin-nginx.conf /etc/nginx/nginx.conf
+	cd /
+    rm -rf $PHPMYADMIN_SOURCE
+	if [ ! $WEBSITES_ENABLE_APP_SERVICE_STORAGE ]; then
+        echo "INFO: NOT in Azure, chown for "$PHPMYADMIN_HOME  
+        chown -R www-data:www-data $PHPMYADMIN_HOME
+	fi
+}
+#unzip drupal
+setup_drupal(){
+    # tar drupal
+    cd $DRUPAL_SOURCE 
+    tar -xf drupal.tar.gz -C $DRUPAL_HOME/ --strip-components=1 
+    chmod a+w "$DRUPAL_HOME/sites/default" 
+    mkdir -p "$DRUPAL_HOME/sites/default/files"
+    chmod a+w "$DRUPAL_HOME/sites/default/files"
+    cp "$DRUPAL_HOME/sites/default/default.settings.php" "$DRUPAL_HOME/sites/default/settings.php"
+    chmod a+w "$DRUPAL_HOME/sites/default/settings.php"
+}
+
+# setup server root
+test ! -d "$DRUPAL_HOME" && echo "INFO: $DRUPAL_HOME not found. creating..." && mkdir -p "$DRUPAL_HOME"
+if [ ! $WEBSITES_ENABLE_APP_SERVICE_STORAGE ]; then
+    echo "INFO: NOT in Azure, chown for "$DRUPAL_HOME  
+    chown -R www-data:www-data $DRUPAL_HOME 
+fi 
 
 # setup nginx log dir
 # http://nginx.org/en/docs/ngx_core_module.html#error_log
-sed -i "s|error_log /var/log/nginx/error.log;|error_log stderr;|g" /etc/nginx/nginx.conf
+# sed -i "s|error_log /var/log/error.log;|error_log stderr;|g" /etc/nginx/nginx.conf
 
 echo "INFO: creating /run/php/php7.0-fpm.sock ..."
-rm -f /run/php/php7.0-fpm.sock
+test -e /run/php/php7.0-fpm.sock && rm -f /run/php/php7.0-fpm.sock
 mkdir -p /run/php
 touch /run/php/php7.0-fpm.sock
 chown www-data:www-data /run/php/php7.0-fpm.sock
+chmod 777 /run/php/php7.0-fpm.sock
 
-# setup server root
-mkdir -p "$DRUPAL_HOME"
-chown -R www-data:www-data "$DRUPAL_HOME/"
+DATABASE_TYPE=$(echo ${DATABASE_TYPE}|tr '[A-Z]' '[a-z]')
+if [ "${DATABASE_TYPE}" == "local" ]; then  
+    echo "Starting MariaDB and PHPMYADMIN..."
+    echo 'mysql.default_socket = /run/mysqld/mysqld.sock' >> $PHP_CONF_FILE     
+    echo 'mysqli.default_socket = /run/mysqld/mysqld.sock' >> $PHP_CONF_FILE     
+    #setup MariaDB
+    echo "INFO: loading local MariaDB and phpMyAdmin ..."
+    echo "Setting up MariaDB data dir ..."
+    setup_mariadb_data_dir
+    echo "Setting up MariaDB log dir ..."
+    test ! -d "$MARIADB_LOG_DIR" && echo "INFO: $MARIADB_LOG_DIR not found. creating ..." && mkdir -p "$MARIADB_LOG_DIR"
+    chown -R mysql:mysql $MARIADB_LOG_DIR
+    echo "Starting local MariaDB ..."
+    start_mariadb
 
-# create index.html
-test -d "${DRUPAL_HOME}/index.html" && rm ${DRUPAL_HOME}/index.html 
-echo '<html><head><meta http-equiv="refresh" content="30" /><meta http-equiv="pragma" content="no-cache" /><meta http-equiv="cache-control" content="no-cache" /><title>Installing Drupal</title></head><body>Installing Drupal ... This could be done in minutes. <p>Please refresh your browser and go to  http://[website]/index.php later.</p></body></html>' > ${DRUPAL_HOME}/index.html 
+    echo "Granting user for phpMyAdmin ..."
+    # Set default value of username/password if they are't exist/null.
+    DATABASE_USERNAME=${DATABASE_USERNAME:-phpmyadmin}
+    DATABASE_PASSWORD=${DATABASE_PASSWORD:-MS173m_QN}
+    echo "phpmyadmin username: "$DATABASE_USERNAME    
+    echo "phpmyadmin password: "$DATABASE_PASSWORD    
+    mysql -u root -e "GRANT ALL ON *.* TO \`$DATABASE_USERNAME\`@'localhost' IDENTIFIED BY '$DATABASE_PASSWORD' WITH GRANT OPTION; FLUSH PRIVILEGES;"
+    echo "Installing phpMyAdmin ..."
+    setup_phpmyadmin
+fi
 
-cd $DRUPAL_SOURCE 
-tar -xf drupal.tar.gz -C $DRUPAL_HOME/ --strip-components=1 
-chmod a+w "$DRUPAL_HOME/sites/default" 
-mkdir -p "$DRUPAL_HOME/sites/default/files"
-chmod a+w "$DRUPAL_HOME/sites/default/files"
-cp "$DRUPAL_HOME/sites/default/default.settings.php" "$DRUPAL_HOME/sites/default/settings.php"
-chmod a+w "$DRUPAL_HOME/sites/default/settings.php"
+# setup Drupal
+if test ! -e "$DRUPAL_HOME/sites/default/settings.php"; then
+    echo "Installing Drupal ..."
+    setup_drupal
+fi
 cd $DRUPAL_HOME
 rm -rf $DRUPAL_SOURCE
-
+       
 echo "Starting SSH ..."
 service ssh start
 
@@ -40,6 +116,10 @@ service php7.0-fpm start
 chmod 777 /run/php/php7.0-fpm.sock
 
 echo "Starting Nginx ..."
+mkdir -p /home/LogFiles/nginx
+if test ! -e /home/LogFiles/nginx/error.log; then 
+    touch /home/LogFiles/nginx/error.log
+fi
 /usr/sbin/nginx
 
 
